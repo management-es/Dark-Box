25afae9fd563b37f3f9dcf0469c60c875b4ec015
Edison <serranoedi1995@gmail.com>
2024-08-20 21:06:32 -0500

    se agregan informes y urls

--- app/src/main/java/com/darkbox/InformesActivity.kt
+++ app/src/main/java/com/darkbox/InformesActivity.kt
@@ -1,25 +1,35 @@
 package com.darkbox
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
+import android.view.View
 import android.widget.Button
-import android.widget.EditText
 
 class InformesActivity : AppCompatActivity() {
-
-    private lateinit var edtInformeDetail: EditText
-    private lateinit var btnSubmit: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_informes)
 
-        edtInformeDetail = findViewById(R.id.edtInformeDetail)
-        btnSubmit = findViewById(R.id.btnSubmit)
+        // Obtener referencias a los botones
+        val btnInstalacion: Button = findViewById(R.id.btnInstalacion)
+        val btnMantenimiento: Button = findViewById(R.id.btnMantenimiento)
+        val btnDesmonte: Button = findViewById(R.id.btnDesmonte)
+        val btnInspeccionPreoperacional: Button = findViewById(R.id.btnInspeccionPreoperacional)
 
-        btnSubmit.setOnClickListener {
-            val informeDetail = edtInformeDetail.text.toString()
-            // Implementa la lógica para enviar el informe aquí
+        // Establecer los manejadores de clic para cada botón
+        btnInstalacion.setOnClickListener { openUrl("https://docs.google.com/forms/d/e/1FAIpQLSftpnH2cvuZxNmmXXaWZRubH2eupvZ7vg_Hsmo9T---hWmWgw/viewform") }
+        btnMantenimiento.setOnClickListener { openUrl("https://docs.google.com/forms/d/e/1FAIpQLSeR7fg2GHl1FgBtND2HUl_qrg2rm-G_dyzsD0_9M3fREJqkbw/viewform") }
+        btnDesmonte.setOnClickListener { openUrl("https://docs.google.com/forms/d/e/1FAIpQLSc0UVXmarAU0jm1306axSGH4w6WG47VGVD6DY978Dk6aiYXNg/viewform?pli=1") }
+        btnInspeccionPreoperacional.setOnClickListener { openUrl("https://docs.google.com/forms/d/e/1FAIpQLSd3nJSSKjcfxU2Zrs0vPeejqeXGT53nS9boiWu-J-6f5rPISQ/viewform") }
+    }
+
+    private fun openUrl(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse(url)
         }
+        startActivity(intent)
     }
 }
