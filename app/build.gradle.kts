4d455b3dc2a122c26113b1e78ff7058bddbdd640
Edison <serranoedi1995@gmail.com>
2024-09-08 14:44:57 -0500

    ui con animaciones frontend

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,7 +71,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
+    implementation("com.airbnb.android:lottie:6.5.2")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
 
 
 }
